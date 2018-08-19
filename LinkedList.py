a9fd09401094558f29ab6e2c374a94512730f43b
Prakhar Srivastava <prkhrsrvstv1@gmail.com>
2018-08-19 23:45:16 +0530

    Add some more functionality to LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,58 +1,93 @@
+# This class defines an individual element of the linked list
 class Element(object):
     
     def __init__(self, value):
         self.value = value
         self.next = None
 
-
+# This class defines a linked list and various methods for manipulating it
 class LinkedList(object):
     
     def __init__(self, head=None):
         self.head = head
     
+    # Adds a new_element to the end of the list
     def append(self, new_element):
-        current = self.head
-        if current:
-            while current.next:
-                current = current.next
-            current.next = new_element
-        else:
+        if self.head == None:
             self.head = new_element
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_element
+        return
     
-    def print(self):
-        if self.head:
-            current = self.head
-            while True:
-                print(current.value, end=' ')
-                current = current.next
-                if current:
-                    print("-->", end=' ')
-                else:
-                    print("--||")
-                    break
-        else:
-            print("Can't print. List is empty.")
+    # Inserts the new_element at index(th) position in the list
+    def insert(self, new_element, index):
+        if index == 1:
+            new_element.next = self.head
+            self.head = new_element
+            return
+        if self.head == None:
+            print("ERROR: Can't insert at index:" + str(index) + ". List is empty.")
+            return
+        current = self.head
+        counter = 1
+        while current and counter < index-1:
+            current = current.next
+            counter += 1
+        if not current:
+            print("Insert unsuccesful. Index out of bounds.")
+            return
+        new_element.next = current.next
+        current.next = new_element
+        return
+    
+    # Deletes the element at the index(th) position
+    def delete(self, index):
+        if not self.head:
+            print("Can't delete. List is empty.")
+            return
+        if index == 1:
+            self.head = self.head.next
+            return
+        current = self.head
+        counter = 1
+        while current and counter < index-1:
+            current = current.next
+            counter += 1
+        if not current or not current.next:
+            print("Delete unsuccessful. Ran out of bounds.")
+            return
+        current.next = current.next.next
 
-
-def __main__():
-    l = LinkedList()
-    l.print()
-    e1 = Element(3)
-    e2 = Element(99)
-    e3 = Element('hey there!')
-    e4 = Element('Uncle')
-    e5 = Element([3, 2, 6, 4, '3eer4', 2.442])
-    l.append(e1)
-    l.print()
-    l.append(e2)
-    l.print()
-    l.append(e3)
-    l.print()
-    l.append(e4)
-    l.print()
-    l.append(e5)
-    l.print()
-
-
-if __name__ == "__main__":
-    __main__()
+    # Deletes an element from the end of the list and returns it
+    def pop(self):
+        if not self.head:
+            print("Can't pop. List is empty.")
+            return
+        current = self.head
+        if current.next == None:
+            popped_element = current
+            self.head = None
+            return popped_element
+        while current.next.next:
+            current = current.next
+        popped_element = current.next
+        current.next = None
+        return popped_element
+    
+    # Prints the entire list
+    def display(self):
+        if not self.head:
+            print("Can't display. List is empty.")
+            return
+        current = self.head
+        while current:
+            print(current.value, end='')
+            current = current.next
+            if current:
+                print("-->", end='')
+            else:
+                print()
+        return
